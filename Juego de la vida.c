2b11c9de68e4ffd6dc17cf4457f5b76d4667b4ba
Derek Cortez <derekcortez16@gmail.com>
2019-03-02 22:00:02 -0600

    Add files via upload

--- Juego de la vida.c
+++ Juego de la vida.c
@@ -3,29 +3,36 @@
 #include <time.h>
 
 
-int pideMatriz(int filas, int columnas);
+int** pideMatriz(int filas, int columnas);
 void imprimeMatriz(int** matriz, int filas, int columnas);
+void checador(int **matriz, int **matriz2, int filas, int columnas);
 int vecinasVivas(int** matriz, int, int);
 
 
-int main(){
+int main()
+{
     int filas , columnas;
 	printf("Numero de filas de la matriz\n");
 	scanf("%d",&filas);
 	printf("Numero de columnas de la matriz\n");
 	scanf("%d",&columnas);
 	int **matriz = pideMatriz(filas, columnas);
+	int **matriz2= pideMatriz(filas,columnas);
 	imprimeMatriz(matriz, filas, columnas);
+	free(matriz);
 	return 0;
 }
 
-int pideMatriz(filas, columnas){
+int** pideMatriz(filas, columnas)
+{
 	int i, j;
 	int **matriz;
-	
-	matriz = (int **)malloc(filas*sizeof(int*));
-	
-	for(i=0; i<filas; i++)
+
+	srand(time(NULL));
+	//regresa un arreglo de n elementos, cada elemento es un apuntador
+	matriz = (int **)malloc(filas*sizeof(int*)); 
+	//crea la matriz, hace un arreglo para cada elemento del arreglo inicial
+	for(i=0; i<filas; i++) 
 	{
 		matriz[i] = (int *)malloc(columnas*sizeof(int));
 	}
@@ -54,21 +61,42 @@ void imprimeMatriz(int** matriz, int filas, int columnas){
 		printf("\n");
 	}
 }
+void checador(int **matriz, int **matriz2, int filas, int columnas)
+{	
+	int i,j;
+    for(i = 0; i <filas; i++)
+    {
+        for(j = 0; j <columnas; j++)
+        {
+            if(matriz[i][j] == ". " && VVivas(matriz,i, j) == 3)
+            {
+                matriz2[i][j] = "@ ";
+            }
+            else if(matriz[i][j] == "@ " && (VVivas(matriz,i, j) == 3 || VVivas(** matriz,i,j)==2))
+            {
+                matriz2[i][j] = "@ ";
+            }
+            else
+            {
+                matriz2[i][j] = ". ";
+            }
+        }
+    }
+}
 
-int vecinasVivas(int **matriz, int filas, int columnas)
+int VVivas(int **matriz, int filas, int columnas)
 {
     int total = 0;
 	int i,j;
-            for(i = filas - 1; i <= filas + 1; i++)
+    for(i = filas - 1; i <= filas + 1; i++)
+    {
+    	for(j = columnas - 1; j <= columnas + 1; j++)
+        {
+        	if (i >= 0 && j >= 0 && i < filas && j < columnas)
             {
-                for(j = columnas - 1; j <= columnas + 1; j++)
-                {
-                    if (i >= 0 && j >= 0 && i < filas && j < columnas)
-                    {
-                        total += matriz[i][j];
-                    }
-                }
+            	total += matriz[i][j];
             }
-
-            return total - matriz[filas][columnas];
+        }
+    }
+	return total - matriz[filas][columnas];
 }
